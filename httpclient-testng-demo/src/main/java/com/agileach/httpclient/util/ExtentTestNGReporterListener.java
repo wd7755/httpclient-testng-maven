bd29a057303eaa4b585d1502756d24b57e27cad4
wd7755 <w_d775@sina.com>
2020-02-08 22:39:37 +0800

    Refactor code

--- httpclient-testng-demo/src/main/java/com/agileach/httpclient/util/ExtentTestNGReporterListener.java
+++ httpclient-testng-demo/src/main/java/com/agileach/httpclient/util/ExtentTestNGReporterListener.java
@@ -114,7 +114,7 @@ public class ExtentTestNGReporterListener implements IReporter {
         htmlReporter.config().setChartVisibilityOnOpen(true);
         htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
         htmlReporter.config().setTheme(Theme.STANDARD);
-        htmlReporter.config().setEncoding("GBK");
+        //htmlReporter.config().setEncoding("GBK");
         htmlReporter.config().setCSS(".node.level-1  ul{ display:none;} .node.level-1.active ul{display:block;}");
         
         extent = new ExtentReports();
