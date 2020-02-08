c20e0410fa2d1af93c5522b2ce911173035c32ac
wd7755 <w_d775@sina.com>
2020-02-08 11:54:02 +0800

    refactor codes and files

--- httpclient-testng-demo/src/main/java/com/agileach/httpclient/util/RetryListener.java
+++ httpclient-testng-demo/src/main/java/com/agileach/httpclient/util/RetryListener.java
@@ -9,7 +9,7 @@ import org.testng.annotations.ITestAnnotation;
 public class RetryListener implements IAnnotationTransformer {
     public void transform(ITestAnnotation annotation, Class testClass,
             Constructor testConstructor, Method testMethod) {
-        IRetryAnalyzer retry = annotation.getRetryAnalyzer();
+        Class<? extends IRetryAnalyzer> retry = annotation.getRetryAnalyzerClass();
         if (retry == null) {
             annotation.setRetryAnalyzer(Retry.class);
         }
